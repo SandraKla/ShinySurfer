ffe4918fb2339f7425752d3cc35a968b34c666b3
SandraKla <sandra13897@wp.pl>
2021-01-05 22:05:07 +0100

    Update Design

--- app.R
+++ app.R
@@ -24,32 +24,29 @@ load("desterieux_3d.rda")
 ui <- fluidPage(
   dashboardPage(
     
-    dashboardHeader(title="ggseg3d"),
-    dashboardSidebar(
-      sidebarMenu(
+    dashboardHeader(title="ShinySurfer", titleWidth = 350),
+    dashboardSidebar( width = 350, 
+      sidebarMenu(id="sideM", 
         
-        menuItem("Data",tabName = "Data",icon = icon("file"),
-                 fileInput("data_table","TableInput",accept = c("xlsx","xls")),
-                 prettyRadioButtons("data_type",label = "Data File Type",choices = c("FreeSurfer","CERES","Others"),inline=TRUE)),
+        menuItem("Home", tabName = "home", icon = icon("home"), br()),
         
-        
-        menuItem("Atlas",tabName = "Atlas",icon=icon("file-alt"),
+        menuItem("Upload Data",tabName = "updata", icon = icon("file"),
+                 prettyRadioButtons("data_type",label = "Data File Type",
+                                    choices = c("FreeSurfer","CERES","Others"), inline = T),
+                 fileInput("data_table","Upload data",accept = c("xlsx","xls")),
+                 
                  conditionalPanel(
                    condition="output.dataFileLoad==true",
-                   fileInput("name_file","Thinkness Names correction",accept = c("xlsx","xls"))
-                 )
-        ),
+                   fileInput("name_file","Names correction", accept = c("xlsx","xls"))
+                 )),
         
-        menuItem("Quality Control",tabName = "qc",icon=icon("chart-bar"),expandedName = "qc",
+        menuItem("Quality Control",tabName = "qc",icon=icon("chart-bar"), expandedName = "qc",
                  conditionalPanel(
                    condition="output.dataFileLoad==true",
                    uiOutput("fil_qc"),
                    uiOutput("qc_kon"),
-                   actionButton("dp","Distribution Plot")
-                 )
-        ),
-        
-        
+                   actionButton("dp","Make Quality Raincloud")
+                 )),
         
         menuItem("Descriptive Statistics",expandedName = "ds",icon=icon("brain"),
                  conditionalPanel(
@@ -71,46 +68,28 @@ ui <- fluidPage(
                    
                    checkboxInput("col","Color and Value",FALSE),
                    
-                   conditionalPanel(condition="input.col==1",
-                                    colourInput("color_obergrenze", "Please select the color of the upper bound", "red"),
-                                    numericInput(inputId = 'wert_obergrenze',
-                                                 label = 'Please choose a value for the upper bound',
-                                                 4.2),
-                                    
-                                    colourInput("color_untergrenze", "Please select the color of the lower bound", "blue"),
-                                    numericInput(inputId = 'wert_untergrenze',
-                                                 label = 'Please choose a value for the lower bound',
-                                                 1.5),
-                                    
-                                    
-                                    
-                                    actionButton("add_mitte", "Add new values and colors"),
-                                    hr(),
-                                    actionButton("remove_mitte","Remove"),
-                                    hr()
-                   ),
-                   checkboxInput("com","composity",value = TRUE),
-                   checkboxInput("check_download","Download",FALSE),
+                   checkboxInput("com","Composity",value = TRUE),
+                   #checkboxInput("check_download","Download",FALSE),
                    
-                   actionButton("ab","Brain Map"),
-                   conditionalPanel(
-                     condition = "input.check_download==1",
-                     textInput(inputId = "name",
-                               "file name"
-                     ),
-                     
-                     selectInput(inputId = "format",
-                                 "file format",
-                                 choices = c("svg","pdf","png"),
-                                 "pdf"
-                     ),
-                     div(style="height:100px;",
-                         checkboxInput("down_filter","With Filer Label",value = FALSE)
-                         
-                     ),
-                     actionButton("download","Download image"),
-                     hr()
-                   )
+                   actionButton("ab","Brain Map")
+                   #conditionalPanel(
+                   #  condition = "input.check_download==1",
+                   #  textInput(inputId = "name",
+                   #            "file name"
+                   # ),
+                   #  
+                   #  selectInput(inputId = "format",
+                   #              "file format",
+                   #              choices = c("svg","pdf","png"),
+                   #              "pdf"
+                   # ),
+                   #  div(style="height:100px;",
+                   #      checkboxInput("down_filter","With Filer Label",value = FALSE)
+                   #      
+                   # ),
+                   #  actionButton("download","Download image"),
+                   #  hr()
+                   #)
                  )
         ),
         menuItem("Linear Regression",expandedName = "ss",icon=icon("chart-line"),
@@ -135,17 +114,31 @@ ui <- fluidPage(
         
         tags$button("Restart", 
                     id="restart", 
-                    type="button", class="btn btn-danger action-button", onclick="history.go(0)"),
-        id="sideM"
+                    type="button", class="btn btn-danger action-button", onclick="history.go(0)")
+        
         
       )
     ),  
     
     
     dashboardBody(
+
       conditionalPanel(
         condition="output.dataFileLoad==true && input.sidebarItemExpanded=='qc'",
-        uiOutput("qc_tabs")
+        uiOutput("qc_tabs")),
+      
+      conditionalPanel(condition="input.col==1 && input.sidebarItemExpanded=='ds'",
+                       colourInput("color_obergrenze", "Please select the color of the upper bound", "red"),
+                       numericInput(inputId = 'wert_obergrenze',
+                                    label = 'Please choose a value for the upper bound',
+                                    4.2),
+                       
+                       colourInput("color_untergrenze", "Please select the color of the lower bound", "blue"),
+                       numericInput(inputId = 'wert_untergrenze',
+                                    label = 'Please choose a value for the lower bound',
+                                    1.5),
+                       actionButton("add_mitte", "Add new values and colors"),
+                       actionButton("remove_mitte","Remove")
       ),
       
       conditionalPanel(
@@ -175,6 +168,14 @@ ui <- fluidPage(
           tabPanel("Table",DT::dataTableOutput("ls_table")),
           tabPanel("Lasso tabel",DT::dataTableOutput("lasso_table"))
         )
+      ),
+      
+      conditionalPanel(
+        condition = "!(input.sidebarItemExpanded=='qc')
+                    && !(input.sidebarItemExpanded=='ss')
+                    && !(input.sidebarItemExpanded=='ds')
+                    && !(input.sidebarItemExpanded=='ls')",
+        includeMarkdown("Home.md")
       )
     )
   )
